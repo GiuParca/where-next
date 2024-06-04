a4b0ec5235e908fa12ef142bf0dc08947374f04d
Giulia Parca <124640350+GiuParca@users.noreply.github.com>
2024-06-04 23:08:03 +0100

    Adding a filter to the search bar

--- script.js
+++ script.js
@@ -1,30 +1,49 @@
+const searchBar = document.getElementById('input');
+let cities = []; 
+
+function renderCards(data) {
+    const container = document.getElementById('container');
+    container.innerHTML = ''; 
+
+    data.forEach(city => {
+        const card = document.createElement('div');
+        card.classList.add("card");
+
+        const cityName = document.createElement('h2');
+        cityName.textContent = city.city;
+        card.classList.add("city");
+
+        const img = document.createElement('img');
+        img.src = city.img;
+        img.alt = city.city;
+
+        const countryName = document.createElement('h3');
+        countryName.textContent = city.country;
+        card.classList.add("country");
+
+        card.appendChild(cityName);
+        card.appendChild(img);
+        card.appendChild(countryName);
+        container.appendChild(card);
+    });
+}
+
 function loadingCards() {
     fetch('./data.json')
         .then(response => response.json())
         .then(data => {
-            const cities = data.cities;
-            const container = document.getElementById('container');
-            cities.forEach(city => {
-                const card = document.createElement('div');
-                card.classList.add("card");
-
-                const cityName = document.createElement('h2');
-                cityName.textContent = city.city;
-
-                const img = document.createElement('img');
-                img.src = city.img;
-                img.alt = city.city;
-
-                const countryName = document.createElement('h3');
-                countryName.textContent = city.country;
-
-                card.appendChild(cityName);
-                card.appendChild(img);
-                card.appendChild(countryName);
-                container.appendChild(card);
-            });
+            cities = data.cities; 
+            renderCards(cities); 
         })
-        .catch(error => console.error('Error loading the cards'));
+        .catch(error => console.error('Error loading the cards', error));
 }
 
 loadingCards();
+
+searchBar.addEventListener('keyup', (e) => {
+    const searchTarget = e.target.value.toLowerCase();
+    const filteredResult = cities.filter(city => {
+        return city.city.toLowerCase().includes(searchTarget) || city.country.toLowerCase().includes(searchTarget);
+    });
+    renderCards(filteredResult);
+});
